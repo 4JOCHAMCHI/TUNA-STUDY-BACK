1b8621908f4a00e77aa5a2b77c5b6b5ed68564e0
chosoobin37 <angelajsb@naver.com>
2024-06-26 19:09:28 +0900

    first commit

---
+++ src/main/java/com/team4chamchi/tunastudy/TunaStudyBackApplication.java
@@ -0,0 +1,13 @@
+package com.team4chamchi.tunastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunaStudyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunaStudyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team4chamchi/tunastudy/TunaStudyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team4chamchi.tunastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TunaStudyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
