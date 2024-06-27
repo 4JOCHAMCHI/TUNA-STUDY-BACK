f2cc6db16caa861c7cb93eaaa0ca8640f758a0a6
chosoobin37 <angelajsb@naver.com>
2024-06-27 13:45:49 +0900

    Chore: ModelMapper build.gradle add

--- src/main/java/com/team4chamchi/tunastudy/TunaStudyBackApplication.java
+++ src/main/java/com/team4chamchi/tunastudy/TunaStudyBackApplication.java
@@ -2,6 +2,8 @@ package com.team4chamchi.tunastudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 public class TunaStudyBackApplication {
@@ -10,4 +12,8 @@ public class TunaStudyBackApplication {
         SpringApplication.run(TunaStudyBackApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
