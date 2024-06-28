e4ebe1139bcc687289e7a63e029dd983e4712e2a
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2024-06-28 17:20:05 +0900

    Feat: 좌석 전체 조회

--- src/main/java/com/team4chamchi/tunastudy/reservation/controller/ReservationController.java
+++ src/main/java/com/team4chamchi/tunastudy/reservation/controller/ReservationController.java
@@ -2,11 +2,12 @@ package com.team4chamchi.tunastudy.reservation.controller;
 
 import com.team4chamchi.tunastudy.reservation.dto.ReservationDTO;
 import com.team4chamchi.tunastudy.reservation.service.ReservationService;
+import com.team4chamchi.tunastudy.studyroom.dto.StudyRoomDTO;
+import com.team4chamchi.tunastudy.studyroom.service.StudyRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,10 +16,12 @@ import java.util.List;
 public class ReservationController {
 
     private final ReservationService reservationService;
+    private final StudyRoomService studyRoomService;
 
     @Autowired
-    public ReservationController(ReservationService reservationService) {
+    public ReservationController(ReservationService reservationService, StudyRoomService studyRoomService) {
         this.reservationService = reservationService;
+        this.studyRoomService = studyRoomService;
     }
 
     @GetMapping("/reservation/{phone}/{roomId}")
@@ -33,5 +36,12 @@ public class ReservationController {
         List<ReservationDTO> reservationList = reservationService.findAllAvailableSeat();
 
         return ResponseEntity.ok(reservationList);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<StudyRoomDTO>> findAllSeat() {
+        List<StudyRoomDTO> allSeatList = studyRoomService.findALlSeat();
+
+        return ResponseEntity.ok(allSeatList);
     }
 }
