f3f4703cf647b4914ed310c36300c8c847eb2082
goeunpark123 <goeunpark21@gmail.com>
2024-06-29 16:32:36 +0900

    Feat: 예약/퇴실 문자 발송

--- src/main/java/com/team4chamchi/tunastudy/reservation/service/ReservationService.java
+++ src/main/java/com/team4chamchi/tunastudy/reservation/service/ReservationService.java
@@ -2,6 +2,7 @@ package com.team4chamchi.tunastudy.reservation.service;
 
 import com.team4chamchi.tunastudy.member.aggregate.Member;
 import com.team4chamchi.tunastudy.member.repository.MemberRepository;
+import com.team4chamchi.tunastudy.notification.service.NotificationService;
 import com.team4chamchi.tunastudy.reservation.aggregate.Reservation;
 import com.team4chamchi.tunastudy.reservation.dto.ReservationDTO;
 import com.team4chamchi.tunastudy.reservation.repository.ReservationRepository;
@@ -19,14 +20,16 @@ import java.util.stream.Collectors;
 public class ReservationService {
 
     private final StudyRoomRepository studyRoomRepository;
+    private final NotificationService notificationService;
     private MemberRepository memberRepository;
     private ReservationRepository reservationRepository;
 
     @Autowired
-    public ReservationService(MemberRepository memberRepository, ReservationRepository reservationRepository, StudyRoomRepository studyRoomRepository) {
+    public ReservationService(MemberRepository memberRepository, ReservationRepository reservationRepository, StudyRoomRepository studyRoomRepository, NotificationService notificationService) {
         this.memberRepository = memberRepository;
         this.reservationRepository = reservationRepository;
         this.studyRoomRepository = studyRoomRepository;
+        this.notificationService = notificationService;
     }
 
     public List<ReservationDTO> findAllAvailableSeat() {
@@ -39,6 +42,7 @@ public class ReservationService {
         return member.orElseGet(() -> {
             //조회해서 멤버가 없으면 생성
             Member newMember = new Member(memberPhone);
+
             return memberRepository.save(newMember);
         });
     }
@@ -48,6 +52,9 @@ public class ReservationService {
         Member member = findMemberByPhone(memberPhone);
         StudyRoom room = studyRoomRepository.findById(roomId).orElseThrow(() -> new RuntimeException("유효하지 않은 좌석입니다."));
 
+        String phone = "+82" + member.getMemberPhone();
+        String roomName = room.getRoomName();
+
         //전화번호랑 좌석으로 예약 조회
         Optional<Reservation> reservation = reservationRepository.findByMember_MemberPhoneAndRoom_RoomId(memberPhone, roomId);
 
@@ -56,15 +63,20 @@ public class ReservationService {
             Reservation newReservation = new Reservation(member, room);
             reservationRepository.save(newReservation);
 
+            notificationService.sendMessage(phone, roomName + "번 좌석 예약되었습니다!");
+
             return new ReservationDTO(reservationRepository.save(newReservation));
         }
 
         //조회된 예약이 있는데 상태가 False -> 예약
         if (!reservation.get().getOccupied()) {
             reservation.get().setOccupied(true);
+            notificationService.sendMessage(phone, roomName + "번 좌석 예약되었습니다!");
+
         } else {
             //조회된 예약이 있는데 상태가 True -> 퇴실
             reservation.get().setOccupied(false);
+            notificationService.sendMessage(phone, roomName + "번 좌석 퇴실되었습니다!");
         }
 
         return new ReservationDTO(reservationRepository.save(reservation.get()));
