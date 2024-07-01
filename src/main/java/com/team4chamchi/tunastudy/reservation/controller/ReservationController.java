2503700abdbe88522e40ec3299825cbd33eda552
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2024-07-01 14:13:19 +0900

    Fix: 오류 처리

--- src/main/java/com/team4chamchi/tunastudy/reservation/controller/ReservationController.java
+++ src/main/java/com/team4chamchi/tunastudy/reservation/controller/ReservationController.java
@@ -2,17 +2,20 @@ package com.team4chamchi.tunastudy.reservation.controller;
 
 import com.team4chamchi.tunastudy.member.aggregate.Member;
 import com.team4chamchi.tunastudy.member.dto.MemberDTO;
+import com.team4chamchi.tunastudy.reservation.aggregate.Reservation;
 import com.team4chamchi.tunastudy.reservation.dto.ReservationDTO;
 import com.team4chamchi.tunastudy.reservation.service.ReservationService;
 import com.team4chamchi.tunastudy.studyroom.dto.StudyRoomDTO;
 import com.team4chamchi.tunastudy.studyroom.service.StudyRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ReservationController {
@@ -42,9 +45,8 @@ public class ReservationController {
 
     @GetMapping("/member/{phone}")
     public ResponseEntity<ReservationDTO> findReservationByPhone(@PathVariable("phone") String phone) {
-        ReservationDTO reservation =  new ReservationDTO(reservationService.findReservationByPhone(phone));
-
-        return ResponseEntity.ok(reservation);
+        Optional<Reservation> reservation = reservationService.findReservationByPhone(phone);
+        return reservation.map(value -> ResponseEntity.ok(new ReservationDTO(value))).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
     }
 
     @GetMapping("/reservation/{phone}/{roomId}")
--- src/main/java/com/team4chamchi/tunastudy/reservation/service/ReservationService.java
+++ src/main/java/com/team4chamchi/tunastudy/reservation/service/ReservationService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -37,10 +36,8 @@ public class ReservationService {
         return reservationRepository.findByOccupiedTrue().stream().map(ReservationDTO::new).collect(Collectors.toList());
     }
 
-    public Reservation findReservationByPhone(String memberPhone) {
-        Optional<Reservation> reservation = reservationRepository.findByMember_MemberPhoneAndOccupiedTrue(memberPhone);
-
-        return reservation.orElse(null);
+    public Optional<Reservation> findReservationByPhone(String memberPhone) {
+        return reservationRepository.findByMember_MemberPhoneAndOccupiedTrue(memberPhone);
     }
 
     public Member findMemberByPhone(String memberPhone) {
